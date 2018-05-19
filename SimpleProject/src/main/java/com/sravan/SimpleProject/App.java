d175a4086360e6c7a2f8f67e8401387597ce678a
Sravan <sravn@gmail.com>
2018-05-19 18:59:47 +0530

    sample maven project-first commit

---
+++ SimpleProject/src/main/java/com/sravan/SimpleProject/App.java
@@ -0,0 +1,14 @@
+package com.sravan.SimpleProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Sravan(chinky)");
+    }
+}
